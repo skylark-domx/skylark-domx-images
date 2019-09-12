d39a90d9649dad4af5fd1bcbb50eca91844de828
hudaokeji <github@hudaokeji.com>
2019-09-13 01:28:15 +0900

    moved from skylark-utils-dom

--- dist/skylark-domx-images/main.js
+++ dist/skylark-domx-images/main.js
@@ -1,9 +1,9 @@
 /**
- * skylark-domx-browser - The skylark browser library for dom api extension.
+ * skylark-domx-images - The skylark image library for dom api extension.
  * @author Hudaokeji Co.,Ltd
  * @version v0.9.0
  * @link www.skylarkjs.org
  * @license MIT
  */
-define(["./browser"],function(e){return e});
+define(["./images"],function(e){return e});
 //# sourceMappingURL=sourcemaps/main.js.map
